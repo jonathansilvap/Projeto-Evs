33ce600bfb3b877a958a4c9ca63005e83fc00347
jonathansilvap <jonathan.silvap2012@gmail.com>
2023-12-27 16:02:27 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Evs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_Evs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
